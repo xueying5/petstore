58397576d91d750d71e1e29418fa9c35cc117b4e
举南 马 <majunan@foxmail.com>
2022-11-10 21:46:02 +0800

    chapter 7

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import Form from "@/components/Form";
+import Main from "@/components/Main";
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'iMain',
+    component: Main
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/form',
+    name: 'Form',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: Form
   }
 ]
 
