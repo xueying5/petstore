657036ae2ac54475c632b88acae188227b1e7d2b
举南 马 <majunan@foxmail.com>
2022-11-12 16:35:06 +0800

    添加动画

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,46 +1,42 @@
 <template>
   <div>
-  <my-header :cartItemCount="cartItemCount"></my-header>
-  <main>
-    <div v-for="product in sortedProducts">
-      <div class="row">
-        <div class="col-md-5 col-md-offset-0">
-          <figure>
-            <img class="product" v-bind:src="product.image" >
-          </figure>
-        </div>
-        <div class="col-md-6 col-md-offset-0 description">
-          <router-link tag="h1" :to="{ name : 'Id', params: {id: product.id}}" >{{product.title}}</router-link>
-          <p v-html="product.description"></p>
-          <p class="price">
-            {{product.price | formatPrice}}
-          </p>
-          <button class=" btn btn-primary btn-lg"
-          v-on:click="addToCart(product)"
-          v-if="canAddToCart(product)">Add to cart</button>
-          <button disabled="true" class=" btn btn-primary btn-lg"
-          v-else >Add to cart</button>
-          <span class="inventory-message"
-          v-if="product.availableInventory - cartCount(product.id) === 0">All Out!
-        </span>
-        <span class="inventory-message"
-        v-else-if="product.availableInventory - cartCount(product.id) < 5">
-        Only {{product.availableInventory - cartCount(product.id)}} left!
-      </span>
-      <span class="inventory-message"
-      v-else>Buy Now!
-    </span>
-    <div class="rating">
-      <span  v-bind:class="{'rating-active' :checkRating(n, product)}"
-      v-for="n in 5" >☆
-    </span>
+    <my-header :cartItemCount="cartItemCount"></my-header>
+    <main>
+      <div v-for="product in sortedProducts">
+        <div class="row">
+          <div class="col-md-5 col-md-offset-0">
+            <figure>
+              <img class="product" v-bind:src="product.image">
+            </figure>
+          </div>
+          <div class="col-md-6 col-md-offset-0 description">
+            <router-link tag="h1" :to="{ name: 'Id', params: { id: product.id } }">{{ product.title }}</router-link>
+            <p v-html="product.description"></p>
+            <p class="price">
+              {{ product.price | formatPrice }}
+            </p>
+            <button class=" btn btn-primary btn-lg" v-on:click="addToCart(product)" v-if="canAddToCart(product)">Add to
+              cart</button>
+            <button disabled="true" class=" btn btn-primary btn-lg" v-else>Add to cart</button>
+            <transition name="bounce" mode="out-in">
+              <span class="inventory-message" key="0" v-if="product.availableInventory - cartCount(product.id) === 0">All Out!
+              </span>
+              <span class="inventory-message" key="" v-else-if="product.availableInventory - cartCount(product.id) < 5">
+                Only {{ product.availableInventory - cartCount(product.id) }} left!
+              </span>
+              <span class="inventory-message" key="" v-else>Buy Now!
+              </span>
+            </transition>
+            <div class="rating">
+              <span v-bind:class="{'rating-active' :checkRating(n, product)}" v-for="n in 5">☆
+              </span>
+            </div>
+          </div><!-- end of col-md-6-->
+        </div><!-- end of row-->
+        <hr />
+      </div><!-- end of v-for-->
+    </main>
   </div>
-</div><!-- end of col-md-6-->
-</div><!-- end of row-->
-<hr />
-</div><!-- end of v-for-->
-</main>
-</div>
 </template>
 <script>
 import MyHeader from './Header.vue';
@@ -114,7 +110,7 @@ export default {
       }
     }
   },
-  created: function() {
+  created: function () {
     axios.get('/static/products.json').then(response => {
       this.products = response.data.products;
       console.log(this.products);
@@ -123,5 +119,35 @@ export default {
 };
 </script>
 <style scoped>
+.bounce-enter-active {
+  animation: shake .72s cubic-bezier(.37, .07, .19, .97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+}
 
+@keyframes shake {
+
+  10%,
+  90% {
+    color: red;
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    color: red;
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
 </style>
